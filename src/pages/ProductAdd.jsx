3551e343b8606fb20847999ad465f3e70a0ce5b6
Soumya <soumyaranjanmishra29@gmail.com>
2025-02-14 14:23:56 +0530

    fix: bug fixes

--- src/pages/ProductAdd.jsx
+++ src/pages/ProductAdd.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "../styles/userProfile.module.css";
+import style from "../styles/UserProfile.module.css";
 import PreHeader from "../component/PreHeader";
 import Footer from "../component/Footer";
 import HeaderNav from "../component/HeaderNavigation";
